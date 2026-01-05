d1634e544a97f7367991fc869118364861f75ff0
photoszzt <zhitingzhu@outlook.com>
2026-01-05 00:40:32 +0000

    Deploying to gh-pages from @ photoszzt/photoszzt.github.io@495dcac04a78b273a34c117d2464302bf4c9457a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-megascale",
+        },{id: "post-mdp",
+        
+          title: "MDP",
+        
+        description: "Markov Decision Process",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/mdp/";
+          
+        },
+      },{id: "post-megascale",
         
           title: "Megascale",
         
